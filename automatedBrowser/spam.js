97def22548ceb3f5fec6fa15d8de4eaad7a757d3
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-08-10 23:27:34 +0500

    add spam message support

---
+++ automatedBrowser/spam.js
@@ -0,0 +1,260 @@
+// Import puppeteer
+// const puppeteer = require( "puppeteer")
+import { setTimeout } from "node:timers/promises";
+import puppeteer from "puppeteer-extra";
+
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+import config from "./../config.json" with { type: "json" };
+puppeteer.use(StealthPlugin());
+
+(async () => {
+  // Launch the browser
+  const browser = await puppeteer.launch({
+    headless: process.env.NODE_ENV === "production",
+    ...(process.platform === "linux" && {
+      args: ["--no-sandbox"],
+      executablePath: "/usr/bin/chromium-browser",
+    }),
+    devtools: true,
+  });
+
+  // Create a page
+  const page = await browser.newPage();
+
+  // Go to your site
+  await page.exposeFunction("logFromPage", (msg) => {
+    console.log("[Browser Log]:", msg);
+  });
+
+  await page.goto("https://revolt.onech.at/", { waitUntil: "networkidle2" });
+
+  await page.screenshot({ path: "app.png" });
+
+  const three = await page.evaluate(
+    async (token, CONFIG) => {
+      const REVOLT_API_BASE_URL = "https://revolt-api.onech.at";
+      const cacheStore = [];
+      const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+      await window.logFromPage("Establishing socket!");
+
+      const ws = new WebSocket(`wss://revolt-ws.onech.at?token=${token}`);
+
+      ws.addEventListener("open", async (event) => {
+        try {
+          console.log("Connection opened!");
+
+          await window.logFromPage("Connection opened!");
+
+          // console.log(ws.bufferedAmount);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+
+      ws.addEventListener("message", async (event) => {
+        try {
+          console.log("Something received over socket");
+
+          const json = JSON.parse(event.data);
+          console.log(json);
+
+          if (json.type === "Error") {
+            console.log(event);
+            console.log(event.data);
+            console.log("Error occured!!!");
+          }
+
+          if (json.type === "Ready") {
+            console.log(
+              "Client became ready, starting to ping every 10 seconds!"
+            );
+
+            setInterval(() => {
+              console.log("Pinging socket!");
+
+              ws.send(
+                JSON.stringify({
+                  type: "Ping",
+                  data: Date.now(),
+                })
+              );
+            }, 1000 * 10);
+
+            const readyData = [];
+
+            const textChannels = json.channels.filter(
+              (c) => c.channel_type === "TextChannel"
+            );
+
+            for (const textChannel of textChannels) {
+              const server = json.servers.find(
+                (s) => s._id === textChannel.server
+              );
+
+              console.log(server);
+
+              if (!server) continue;
+
+              const category = server.categories.find((cat) =>
+                cat.channels.includes(textChannel._id)
+              );
+              console.log(category);
+
+              if (!category) continue;
+              readyData.push({
+                channelId: textChannel._id,
+                channelName: textChannel.name,
+                serverId: textChannel.server,
+                categoryId: category.id,
+              });
+            }
+
+            cacheStore.push(...readyData);
+
+            await window.logFromPage(cacheStore);
+          }
+
+          if (json.type === "ChannelCreate") {
+            console.log("Channel created!");
+
+            if (json.channel_type !== "TextChannel") return;
+
+            cacheStore.push({
+              channelId: json._id,
+              channelName: json.name,
+              serverId: json.server,
+            });
+
+            //  check if ticket then spam!!!
+
+            if (!CONFIG["ticket-servers"].includes(json.server)) return;
+            if (!json.name.startsWith("ticket-")) return;
+
+            const amount = new Array(CONFIG.messages_to_spam);
+
+            for (const a of amount) {
+              await sendMessage(json);
+              if (CONFIG.delay_between_spam_messages_in_second)
+                await setTimeout(
+                  CONFIG.delay_between_spam_messages_in_second * 1000
+                );
+            }
+          }
+
+          if (json.type === "ChannelDelete") {
+            console.log("Channel deleted!");
+
+            const i = cacheStore.findIndex((c) => c.channelId === json.id);
+
+            if (c === -1)
+              return console.log(
+                `Channel delete received but not found in cache ${json.id}`
+              );
+
+            cacheStore.splice(i, 1);
+          }
+
+          //   if (json.type === "Message") {
+          //     console.log("Message!");
+
+          //     // if (
+          //     //   json.content !==
+          //     //   "If you'd like to close this ticket, use the `/close` command.\nClaiming has currently been disabled."
+          //     // )
+          //     //   return;
+
+          //     if (!json.embeds?.length) return console.log(" no embed");
+
+          //     const channel = cacheStore.find(
+          //       (c) => c.channelId === json.channel
+          //     );
+
+          //     if (!channel) {
+          //       return console.log(
+          //         "Message came but channel not found in cache!!!"
+          //       );
+          //     }
+
+          //     // const categoryText = CONFIG.category_ids[channel.categoryId];
+
+          //     // if (!categoryText) return;
+
+          //     // console.log(categoryText);
+
+          //     const isCorrect = json.content.includes(
+          //       "New ticket has been made!"
+          //     );
+
+          //     if (!isCorrect) return console.log("not correct");
+
+          //     const timer = CONFIG.timers.find(
+          //       (t) => t.serverId === channel.serverId
+          //     );
+
+          //     if (timer)
+          //       setTimeout(async () => {
+          //         sendMessage(channel);
+          //       }, 1000 * timer.delayInSeconds);
+
+          //     if (!timer) await sendMessage(channel);
+          //   }
+        } catch (error) {
+          console.log(error);
+        }
+
+        async function sendMessage(channel) {
+          try {
+            const messageToSend = "/claim";
+
+            await fetch(
+              `${REVOLT_API_BASE_URL}/channels/${channel._id}/messages`,
+              {
+                method: "POST",
+                headers: {
+                  "content-type": "application/json",
+                  "X-Session-Token": token,
+                },
+                body: JSON.stringify({ content: messageToSend }),
+              }
+            );
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      });
+
+      ws.addEventListener("close", async (event) => {
+        console.log("Connectio closed!");
+
+        console.log(event);
+        console.log(event.code);
+        console.log(event.reason);
+        console.log(event.wasClean);
+
+        await window.logFromPage("Connection closed!");
+        await window.logFromPage(event);
+      });
+
+      ws.addEventListener("error", async (event) => {
+        console.log("Error occured");
+
+        await window.logFromPage("Error occured");
+        await window.logFromPage(event);
+
+        console.log(event);
+      });
+
+      return ws;
+    },
+    process.env.REVOLT_USER_TOKEN,
+    config
+  );
+
+  console.log(three);
+
+  // Close browser.
+  //   await browser.close();
+})();
